c7ffaae92de426225498e18b6759b15990fa9d94
mikecerc <mikecerc1@outlook.com>
2023-01-20 19:13:51 -0500

    refactor + begun ui

---
+++ client/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="page-container">
+        <Nav />
+        <slot />
+    </div>
+</template>
+<style lang="scss">
+.page-container {
+    padding: 0;
+    margin: 0;
+    top: 0;
+    left: 0;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    flex: display; 
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ client/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "@/assets/scss/_mixins.scss" as *; @use "@/assets/scss/_variables.scss" as *; @use "@/assets/scss/_global.scss" as *;'
+                }
+            }
+        }
+    },
+})
